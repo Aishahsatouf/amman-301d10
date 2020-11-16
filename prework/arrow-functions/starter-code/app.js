d247666f9067f9ec0ead9b7a7c4220bd19d90b3c
Aishah Satouf <71585705+Aishahsatouf@users.noreply.github.com>
2020-11-16 21:56:49 +0200

    Update app.js

--- prework/arrow-functions/starter-code/app.js
+++ prework/arrow-functions/starter-code/app.js
@@ -225,7 +225,7 @@ console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-// the wiindow object
+// the Student object
 // 2. What is "this" when joe.scopeArrow() is invoked?
 // the window object
 // 3. Explain why "this" is different when an arrow function is used.
